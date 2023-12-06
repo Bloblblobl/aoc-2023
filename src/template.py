b9da5b2cce9d18d3a2b2c1f6cfa597698e24a1de
Saar <saar@codehs.com>
2023-12-05 20:31:12 -0800

    add template

---
+++ src/template.py
@@ -0,0 +1,19 @@
+from utils import parse_input_lines
+
+"""
+"""
+
+def part1():
+    lines = parse_input_lines()
+    return lines
+
+print(f'Part 1: {part1()}')
+
+"""
+"""
+
+def part2():
+    lines = parse_input_lines()
+    return lines
+
+print(f'Part 2: {part2()}')
